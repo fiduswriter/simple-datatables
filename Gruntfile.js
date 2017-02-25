b35ab33c093a92bee247e7ae7d5e59ed10babaed
Karl <mobius1@gmx.com>
2017-02-25 20:31:06 +0000

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		jshint: {
-			all: ['*.js', 'test/*.js']
+			all: ['vanilla-dataTables.js', 'test/*.js']
 		},
 		qunit: {
 			all: ['test/index.html']
