8ed354f339adf446933f9c08f2aff1c4a93236da
Karl <mobius1@gmx.com>
2017-02-25 22:03:51 +0000

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -1,129 +1,67 @@
-(function() {
-	'use strict';
-	var t = document.createElement('table');
-	var data = {
-		"headings": [
-			"Name",
-			"Company",
-			"Ext.",
-			"Start Date",
-			"Email",
-			"Phone No."
-		],
-		"rows": [
-			[
-				"Hedwig F. Nguyen",
-				"Arcu Vel Foundation",
-				"9875",
-				"03/27/2017",
-				"nunc.ullamcorper@metusvitae.com",
-				"070 8206 9605"
-			],
-			[
-				"Genevieve U. Watts",
-				"Eget Incorporated",
-				"9557",
-				"07/18/2017",
-				"Nullam.vitae@egestas.edu",
-				"0800 106980"
-			],
-			[
-				"Kyra S. Baldwin",
-				"Lorem Vitae Limited",
-				"3854",
-				"04/14/2016",
-				"in@elita.org",
-				"0800 237 8846"
-			],
-			[
-				"Stephen V. Hill",
-				"Eget Mollis Institute",
-				"8820",
-				"03/03/2016",
-				"eu@vel.com",
-				"0800 682 4591"
-			],
-			[
-				"Vielka Q. Chapman",
-				"Velit Pellentesque Ultricies Institute",
-				"2307",
-				"06/25/2017",
-				"orci.Donec.nibh@mauriserateget.edu",
-				"0800 181 5795"
-			],
-			[
-				"Ocean W. Curtis",
-				"Eu Ltd",
-				"6868",
-				"08/24/2017",
-				"cursus.et@cursus.edu",
-				"(016977) 9585"
-			],
-			[
-				"Kato F. Tucker",
-				"Vel Lectus Limited",
-				"4713",
-				"11/06/2017",
-				"Duis@Lorem.edu",
-				"070 0981 8503"
-			],
-			[
-				"Robin J. Wise",
-				"Curabitur Dictum PC",
-				"3285",
-				"02/09/2017",
-				"blandit@montesnascetur.edu",
-				"0800 259158"
-			],
-			[
-				"Uriel H. Guerrero",
-				"Mauris Inc.",
-				"2294",
-				"02/11/2018",
-				"vitae@Innecorci.net",
-				"0500 948772"
-			],
-			[
-				"Yasir W. Benson",
-				"At Incorporated",
-				"3897",
-				"01/13/2017",
-				"ornare.elit.elit@atortor.edu",
-				"0391 916 3600"
-			],
-			[
-				"Shafira U. French",
-				"Nisi Magna Incorporated",
-				"5116",
-				"07/23/2016",
-				"metus.In.nec@bibendum.ca",
-				"(018013) 26699"
-			],
-			[
-				"Casey E. Hood",
-				"Lorem Vitae Odio Consulting",
-				"7079",
-				"05/05/2017",
-				"justo.Praesent@sitamet.ca",
-				"0800 570796"
-			],
-			[
-				"Caleb X. Finch",
-				"Elit Associates",
-				"3629",
-				"09/19/2016",
-				"condimentum@eleifend.com",
-				"056 1551 7431"
-			],
-		 ]
-	};
-	
-	document.body.appendChild(t);
-	
-	var dt = new DataTable(t, { data: data });
-	
-	QUnit.module('General');
-	QUnit.test( "init", function( assert ) {
-	  assert.ok( Object.prototype.toString.call(dt) === '[object Object]', "Passed!" );
-	});	
-})();
+QUnit.module("qunit-assert-close plugin unit tests");
+
+QUnit.test("Close Numbers", function(assert) {
+  var halfPi = Math.PI / 2,
+      sqrt2 = Math.sqrt(2);
+
+  assert.close(7, 7, 0);
+  assert.close(7, 7.1, 0.1);
+  assert.close(7, 7.1, 0.2);
+
+  assert.close(3.141, Math.PI, 0.001);
+  assert.close(3.1, Math.PI, 0.1);
+
+  assert.close(halfPi, 1.57, 0.001);
+
+  assert.close(sqrt2, 1.4142, 0.0001);
+
+  assert.close(Infinity, Infinity, 1);
+});
+
+QUnit.test("Distant Numbers", function(assert) {
+  var halfPi = Math.PI / 2,
+      sqrt2 = Math.sqrt(2);
+
+  assert.notClose(6, 7, 0);
+  assert.notClose(7, 7.2, 0.1);
+  assert.notClose(7, 7.2, 0.19999999999);
+
+  assert.notClose(3.141, Math.PI, 0.0001);
+  assert.notClose(3.1, Math.PI, 0.001);
+
+  assert.notClose(halfPi, 1.57, 0.0001);
+
+  assert.notClose(sqrt2, 1.4142, 0.00001);
+
+  assert.notClose(Infinity, -Infinity, 5);
+});
+
+QUnit.test("Close Numbers, percentage based", function(assert) {
+  assert.close.percent(7, 7, 0.0);
+  assert.close.percent(7, 7.1, 2.0);   // ~1.43%
+  assert.close.percent(7, 7.1, 1.5);   // ~1.43%
+  assert.close.percent(7, 7.1, 1.43);  // ~1.43%
+  assert.close.percent(Infinity, Infinity, 1.0);
+  assert.close.percent(Infinity, Infinity, 0.0);
+  assert.close.percent(0, 100, Infinity);
+  assert.close.percent(100, 0, Infinity);
+  assert.close.percent(Infinity, -Infinity, Infinity);
+});
+
+QUnit.test("Distant Numbers, percentage based", function(assert) {
+  assert.notClose.percent(6, 7, 0.0);
+  assert.notClose.percent(7, 7.2, 2.50);  // ~2.777%
+  assert.notClose.percent(7, 7.2, 2.70);  // ~2.777%
+  assert.notClose.percent(7, 7.2, 2.77);  // ~2.777%
+  assert.notClose.percent(0, 100, 100.0);
+  assert.notClose.percent(100, 0, 1000000.0);
+  assert.notClose.percent(Infinity, -Infinity, 100.0);
+});
+
+QUnit.test("close.percent is the same as closePercent", function(assert) {
+  assert.strictEqual( assert.close.percent, assert.closePercent );
+});
+
+QUnit.test("notClose.percent is the same as notClosePercent", function(assert) {
+  assert.strictEqual( assert.notClose.percent, assert.notClosePercent );
+});
