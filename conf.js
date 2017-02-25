528dd821a9b66c9dc3628cd4658518350dad2466
Karl <mobius1@gmx.com>
2017-02-25 18:18:19 +0000

    Update conf.js

--- conf.js
+++ conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dist/*.js' , 'tests/*.js'
+      'vanilla-dataTables.min.js' , 'tests/*.js'
     ],
 
 
