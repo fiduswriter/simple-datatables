1a1f42994b1c547a521cf24becf8ec5da9878c62
Karl <mobius1@gmx.com>
2017-02-26 00:33:43 +0000

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -120,10 +120,37 @@
 	
 	document.body.appendChild(t);
 	
+	var log = [];
+	var testName;
 	var dt = new DataTable(t, { data: data });
 	
 	QUnit.module('General');
 	QUnit.test( "init", function( assert ) {
 	  assert.ok( Object.prototype.toString.call(dt) === '[object Object]', "Passed!" );
+	});	
+	
+	QUnit.done(function (test_results) {
+	  var tests = [];
+	  for(var i = 0, len = log.length; i < len; i++) {
+	    var details = log[i];
+	    tests.push({
+	      name: details.name,
+	      result: details.result,
+	      expected: details.expected,
+	      actual: details.actual,
+	      source: details.source
+	    });
+	  }
+	  test_results.tests = tests;
+
+	  window.global_test_results = test_results;
+	});
+	QUnit.testStart(function(testDetails){
+	  QUnit.log(function(details){
+	    if (!details.result) {
+	      details.name = testDetails.name;
+	      log.push(details);
+	    }
+	  });
 	});	
 })();
