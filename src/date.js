8bf8ce92576ea28b8c57d17c97eba4a78699fd4e
Johannes Wilm <mail@johanneswilm.org>
2019-05-09 11:20:31 +0200

    moment => dayjs

--- src/date.js
+++ src/date.js
@@ -1,7 +1,7 @@
-import moment from "moment"
+import dayjs from "dayjs"
 
 /**
- * Use moment.js to parse cell contents for sorting
+ * Use dayjs to parse cell contents for sorting
  * @param  {String} content     The datetime string to parse
  * @param  {String} format      The format for moment to use
  * @return {String|Boolean}     Datatime string or false
@@ -9,28 +9,26 @@ import moment from "moment"
 export const parseDate = (content, format) => {
     let date = false
 
-    // moment() throws a fit if the string isn't a valid datetime string
-    // so we need to supply the format to the constructor (https://momentjs.com/docs/#/parsing/string-format/)
-
     // Converting to YYYYMMDD ensures we can accurately sort the column numerically
 
     if (format) {
         switch (format) {
         case "ISO_8601":
-            date = moment(content, moment.ISO_8601).format("YYYYMMDD")
+            // ISO8601 is already lexiographically sorted, so we can just sort it as a string.
+            date = content
             break
         case "RFC_2822":
-            date = moment(content, "ddd, MM MMM YYYY HH:mm:ss ZZ").format("YYYYMMDD")
+            date = dayjs(content, "ddd, MM MMM YYYY HH:mm:ss ZZ").format("YYYYMMDD")
             break
         case "MYSQL":
-            date = moment(content, "YYYY-MM-DD hh:mm:ss").format("YYYYMMDD")
+            date = dayjs(content, "YYYY-MM-DD hh:mm:ss").format("YYYYMMDD")
             break
         case "UNIX":
-            date = moment(content).unix()
+            date = dayjs(content).unix()
             break
             // User defined format using the data-format attribute or columns[n].format option
         default:
-            date = moment(content, format).format("YYYYMMDD")
+            date = dayjs(content, format).format("YYYYMMDD")
             break
         }
     }
