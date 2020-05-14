0a77bb50b27fe7c1cf9c5d0dfc9860e496dd8c8c
Brendon Stanton <brendonstanton@icloud.com>
2020-05-14 09:25:01 -0400

    Updating formatting on pull request

--- src/columns.js
+++ src/columns.js
@@ -343,7 +343,7 @@ export class Columns {
         const waitFor = []
 
         // Check for date format
-	const isDate = th.getAttribute("data-type") === "date"
+        const isDate = th.getAttribute("data-type") === "date"
         if (isDate) {
             let format = false
             const formatted = th.hasAttribute("data-format")
@@ -360,7 +360,7 @@ export class Columns {
             Array.from(rows).forEach(tr => {
                 const cell = tr.cells[column]
                 const content = cell.hasAttribute('data-content') ? cell.getAttribute('data-content') : cell.innerText
-                const num = parseFunction(typeof content==="string" && !isDate  ? content.replace(/(\$|,|\s|%)/g, "") : content)
+                const num = parseFunction(typeof content==="string" && !isDate ? content.replace(/(\$|,|\s|%)/g, "") : content)
 
                 if (parseFloat(num) == num) {
                     numeric[n++] = {
