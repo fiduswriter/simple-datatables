4cc2a36d5ed89e6a5726d4a0639d7f29e3909399
Gavin Kennedy <gavin@fuzzysystems.co.nz>
2020-10-30 17:01:28 +1300

    Fix for browsers with no support for NodeList
    This fix should allow transpilers targeting older browsers to work around the lack of NodeList support

--- src/helpers.js
+++ src/helpers.js
@@ -41,7 +41,14 @@ export const createElement = (nodeName, attrs) => {
 
 export const flush = el => {
     if (el instanceof NodeList) {
-        el.forEach(e => flush(e))
+        el.forEach(e => flush(e))        
+    } else if (el instanceof Object) {
+        // IE fix - recognises NodeList as Object
+        if (Object.prototype.hasOwnProperty.call(el, 'length')) {
+            el.forEach(e => flush(e));
+        } else {
+            el.innerHTML = "";
+        }
     } else {
         el.innerHTML = ""
     }
