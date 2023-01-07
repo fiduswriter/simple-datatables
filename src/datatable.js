967d31ee87e8524982f9293f19a9dba900991905
Johannes Wilm <mail@johanneswilm.org>
2023-01-07 13:07:05 +0100

    vdom: insert data

--- src/datatable.js
+++ src/datatable.js
@@ -32,7 +32,6 @@ export class DataTable {
             }
         }
 
-        this.initialSortable = this.options.sortable
         this.initialInnerHTML = this.options.destroyable ? this.dom.innerHTML : "" // preserve in case of later destruction
 
         if (this.options.tabIndex) {
@@ -57,6 +56,8 @@ export class DataTable {
         this.currentPage = 0
         this.onFirstPage = true
         this.headerTable = false
+        this.hasHeadings = false
+        this.hasRows = false
 
         this.columnWidths = []
         this.hiddenColumns = []
@@ -89,7 +90,9 @@ export class DataTable {
         // }
 
 
-        this.data = this.readTableData(this.dom, this.options.data)
+        this.data = this.readTableData(this.options.data, this.dom)
+        this.hasRows = Boolean(this.data.data.length)
+        this.hasHeadings = Boolean(this.data.headings.length)
 
         this.columnSettings = this.readColumnSettings(this.options.columns)
 
@@ -103,7 +106,7 @@ export class DataTable {
         }, 10)
     }
 
-    readTableData(dom, dataOption) {
+    readTableData(dataOption, dom=false) {
         const data = {
             data: [],
             headings: []
@@ -111,14 +114,14 @@ export class DataTable {
         if (dataOption?.data) {
             data.data = dataOption.data.map(row => row.map(cell => ({data: cell,
                 text: cell})))
-        } else if (dom.tBodies.length) {
+        } else if (dom?.tBodies.length) {
             data.data = Array.from(dom.tBodies[0].rows).map(row => Array.from(row.cells).map(cell => ({data: cell.dataset.content || cell.innerHTML,
                 text: cell.innerHTML})))
         }
         if (dataOption?.headings) {
             data.headings = dataOption.headings.map(heading => ({data: heading,
                 sorted: false}))
-        } else if (dom.tHead) {
+        } else if (dom?.tHead) {
             data.headings = Array.from(dom.tHead.querySelectorAll("th")).map(th => {
                 const heading = {data: th.innerHTML,
                     sorted: false}
@@ -127,7 +130,7 @@ export class DataTable {
             })
         } else if (dataOption?.data?.data?.length) {
             data.headings = dataOption.data.data[0].map(_cell => "")
-        } else if (dom.tBodies.length) {
+        } else if (dom?.tBodies.length) {
             data.headings = Array.from(dom.tBodies[0].rows[0].cells).map(_cell => "")
         }
 
@@ -210,10 +213,7 @@ export class DataTable {
         this.body = this.dom.tBodies[0]
         this.head = this.dom.tHead
 
-        this.hasRows = this.body.rows.length > 0
-
         this.headings = []
-        this.hasHeadings = this.head.rows.length > 0
 
         if (this.hasHeadings) {
             this.header = this.head.rows[0]
@@ -280,12 +280,6 @@ export class DataTable {
             template = template.replace("{search}", "")
         }
 
-        // if (this.hasHeadings) {
-        //     // Sortable
-        //     this.renderHeader()
-        // }
-
-
         // Paginator
         const paginatorWrapper = createElement("nav", {
             class: "dataTable-pagination"
@@ -314,15 +308,9 @@ export class DataTable {
 
         // Convert rows to array for processing
         this.rowData = Array.from(this.body.rows)
-        this.activeRows = this.rowData.slice()
-        this.activeHeadings = this.headings.slice()
 
         // // Update
         this.update()
-        //
-        //
-        // this.setColumns()
-        //
         //
         // // Fix height
         this.fixHeight()
@@ -413,7 +401,7 @@ export class DataTable {
                 pages: this.pages})
             t = f + this.pages[current].length
             f = f + 1
-            items = this.searching ? this.searchData.length : this.rowData.length
+            items = this.searching ? this.searchData.length : this.data.data.length
         }
 
         if (this.label && this.options.labels.info.length) {
@@ -507,42 +495,6 @@ export class DataTable {
                 pager.appendChild(frag.cloneNode(true))
             })
         }
-    }
-
-    /**
-     * Render the header
-     * @return {Void}
-     */
-    renderHeader() {
-        this.labels = []
-
-        if (this.headings && this.headings.length) {
-            this.headings.forEach((th, i) => {
-
-                this.labels[i] = th.textContent
-
-                if (th.firstElementChild && th.firstElementChild.classList.contains("dataTable-sorter")) {
-                    th.innerHTML = th.firstElementChild.innerHTML
-                }
-
-                th.sortable = th.getAttribute("data-sortable") !== "false"
-
-                th.originalCellIndex = i
-                if (this.options.sortable && th.sortable) {
-                    const link = createElement("a", {
-                        href: "#",
-                        class: "dataTable-sorter",
-                        html: th.innerHTML
-                    })
-
-                    th.innerHTML = ""
-                    th.setAttribute("data-sortable", "")
-                    th.appendChild(link)
-                }
-            })
-        }
-
-        this.fixColumns()
     }
 
     /**
@@ -643,79 +595,6 @@ export class DataTable {
     }
 
     /**
-     * Set up columns
-     */
-    setColumns() {
-
-        this.rowData.forEach(row => {
-            Array.from(row.cells).forEach(cell => {
-                cell.data = cell.innerHTML
-            })
-        })
-
-        // Check for the columns option
-        if (this.options.columns && this.headings.length) {
-
-            this.options.columns.forEach(data => {
-
-                // convert single column selection to array
-                if (!Array.isArray(data.select)) {
-                    data.select = [data.select]
-                }
-
-                if (data.hasOwnProperty("render") && typeof data.render === "function") {
-                    this.selectedColumns = this.selectedColumns.concat(data.select)
-
-                    this.columnRenderers.push({
-                        columns: data.select,
-                        renderer: data.render
-                    })
-                }
-
-                // Add the data attributes to the th elements
-                data.select.forEach(column => {
-                    const th = this.headings[column]
-                    if (!th) {
-                        return
-                    }
-                    if (data.type) {
-                        th.setAttribute("data-type", data.type)
-                    }
-                    if (data.format) {
-                        th.setAttribute("data-format", data.format)
-                    }
-                    if (data.hasOwnProperty("sortable")) {
-                        th.setAttribute("data-sortable", data.sortable)
-                    }
-
-                    if (data.hasOwnProperty("hidden")) {
-                        if (data.hidden !== false) {
-                            this.columns.hide([column])
-                        }
-                    }
-
-                    if (data.hasOwnProperty("sort") && data.select.length === 1) {
-                        this.columns.sort(data.select[0], data.sort, true)
-                    }
-                })
-            })
-        }
-
-        if (this.hasRows) {
-            this.rowData.forEach((row, i) => {
-                row.dataIndex = i
-                Array.from(row.cells).forEach(cell => {
-                    cell.data = cell.innerHTML
-                })
-            })
-
-            this.columns.rebuild()
-        }
-
-        this.renderHeader()
-    }
-
-    /**
      * Destroy the instance
      * @return {void}
      */
@@ -792,9 +671,9 @@ export class DataTable {
      * Fix column widths
      */
     fixColumns() {
-        const activeHeadings = this.virtualDOM.childNodes.find(node => ["THEAD", "TFOOT"].includes(node.nodeType))
+        const activeHeadings = this.virtualDOM.childNodes.find(node => ["THEAD", "TFOOT"].includes(node.nodeType))?.childNodes
 
-        if ((this.options.scrollY.length || this.options.fixedColumns) && activeHeadings?.childNodes.length) {
+        if ((this.options.scrollY.length || this.options.fixedColumns) && activeHeadings?.length) {
 
             this.columnWidths = []
             const renderOptions = {
@@ -1012,29 +891,9 @@ export class DataTable {
         if (isObject(data)) {
             if (data.headings) {
                 if (!this.hasHeadings && !this.hasRows) {
-                    const tr = createElement("tr")
-                    data.headings.forEach(heading => {
-                        const th = createElement("th", {
-                            html: heading
-                        })
-
-                        tr.appendChild(th)
-                    })
-                    this.head.appendChild(tr)
-
-                    this.header = tr
-                    this.headings = [].slice.call(tr.cells)
-                    this.hasHeadings = true
-
-                    // Re-enable sorting if it was disabled due
-                    // to missing header
-                    this.options.sortable = this.initialSortable
-
-                    // Allow sorting on new header
-                    this.renderHeader()
-
-                    // Activate newly added headings
-                    this.activeHeadings = this.headings.slice()
+                    this.data = this.readTableData(data)
+                    this.hasRows = Boolean(this.data.data.length)
+                    this.hasHeadings = Boolean(this.data.headings.length)
                 }
             }
 
@@ -1046,7 +905,7 @@ export class DataTable {
                 const r = []
                 Object.entries(row).forEach(([heading, cell]) => {
 
-                    const index = this.labels.indexOf(heading)
+                    const index = this.data.headings.indexOf(heading)
 
                     if (index > -1) {
                         r[index] = cell
@@ -1057,13 +916,11 @@ export class DataTable {
         }
 
         if (rows.length) {
-            this.rows.add(rows)
-
+            rows.forEach(row => this.data.data.push(row.map(cell => ({data: cell, text: cell}))))
             this.hasRows = true
         }
 
         this.update()
-        this.setColumns()
         this.fixColumns()
     }
 
@@ -1087,8 +944,8 @@ export class DataTable {
      * @return {void}
      */
     print() {
-        const headings = this.activeHeadings
-        const rows = this.activeRows
+        const headings = this.virtualDOM.childNodes.find(node => ["THEAD", "TFOOT"].includes(node.nodeType))?.childNodes
+        const rows = this.data.data
         const table = createElement("table")
         const thead = createElement("thead")
         const tbody = createElement("tbody")
@@ -1133,13 +990,8 @@ export class DataTable {
      * Show a message in the table
      */
     setMessage(message) {
-        let colspan = 1
-
-        if (this.hasRows) {
-            colspan = this.data.data[0].length
-        } else if (this.activeHeadings.length) {
-            colspan = this.activeHeadings.length
-        }
+        const activeHeadings = this.virtualDOM.childNodes.find(node => ["THEAD", "TFOOT"].includes(node.nodeType))?.childNodes
+        let colspan = activeHeadings?.length || 1
 
         this.wrapper.classList.add("dataTable-empty")
 
